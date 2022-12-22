81a04a9ea155b018938b4e371ecd9a6d37f4a975
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-22 19:41:28 +0545

    feat  : exercise :" 6.3: anecdotes, step1 Implement the functionality for voting anecdotes. The amount of votes must be saved to a Redux-store.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import reducer from "./reducers/anecdoteReducer";
 const App = () => {
   const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const vote = (id) => {
     console.log("vote", id);
+    dispatch({ type: "VOTE", data: { id } });
   };
+  // console.log("vote", vote);
 
   return (
     <div>
