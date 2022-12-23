57ba42e84d565b9a107d72c7f6301906bb0f2b10
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-23 13:33:50 +0545

    feat : 6.8: anecdotes, step6 Separate the rendering of the anecdote list into its own component called AnecdoteList. Move all logic related to voting for an anecdote to this new component.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import { AnecdoteForm } from "./components/AnecdoteForm";
-import { useSelector, useDispatch } from "react-redux";
-import { setVotes, setAnecdote } from "./reducers/anecdoteReducer";
+import { AnecdoteList } from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const sortingAccLikes = anecdotes.sort((a, b) => b.votes - a.votes);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    console.log("vote", id);
-    // dispatch({ type: "VOTE", data: { id } });
-    dispatch(setVotes(id));
-  };
-
-  const addingAnecdotes = (event) => {
-    event.preventDefault();
-    // console.log(event.target, "harey");
-    // console.log("mareyyyyy", event.target.anecdote);
-    const content = event.target.anecdote.value;
-
-    // console.log(content, "this is what coming from the content ");
-    dispatch(setAnecdote(content));
-    // dispatch({ type: "ADD", data: { content } });
-    event.target.anecdote.value = "";
-  };
-  // console.log("vote", vote);
-
   return (
     <div>
       <h2>Anecdotes</h2>
 
-      {/* {anecdotes.map((anecdote) => ( */}
-      {sortingAccLikes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-
-      <h2>create new</h2>
-      <AnecdoteForm addingAnecdotes={addingAnecdotes} />
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   );
 };
--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,11 +1,30 @@
 import React from "react";
-export function AnecdoteForm({ addingAnecdotes }) {
+import { useDispatch } from "react-redux";
+import { setAnecdote } from "../reducers/anecdoteReducer";
+// export function AnecdoteForm({ addingAnecdotes }) {
+export function AnecdoteForm() {
+  const dispatch = useDispatch();
+  const addingAnecdotes = (event) => {
+    event.preventDefault();
+    // console.log(event.target, "harey");
+    // console.log("mareyyyyy", event.target.anecdote);
+    const content = event.target.anecdote.value;
+
+    // console.log(content, "this is what coming from the content ");
+    dispatch(setAnecdote(content));
+    // dispatch({ type: "ADD", data: { content } });
+    event.target.anecdote.value = "";
+  };
+
   return (
-    <form onSubmit={addingAnecdotes}>
-      <div>
-        <input name="anecdote" />
-      </div>
-      <button type="submit">create</button>
-    </form>
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addingAnecdotes}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
   );
 }
