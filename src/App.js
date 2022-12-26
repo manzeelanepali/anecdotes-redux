bc8ee33efde4a3e0e396bdcee90042319e27185f
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-26 15:14:01 +0545

    faet : introducing new reducer notificationReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { AnecdoteForm } from "./components/AnecdoteForm";
 import { AnecdoteList } from "./components/AnecdoteList";
-
+import Notification from "./components/Notification";
 const App = () => {
   return (
     <div>
@@ -8,6 +8,7 @@ const App = () => {
 
       <AnecdoteForm />
       <AnecdoteList />
+      <Notification />
     </div>
   );
 };
