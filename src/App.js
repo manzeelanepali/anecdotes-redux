ac0bb8f5cc35e2bc1e7e5e2a858b3ca7e551247b
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-27 16:03:30 +0545

    feat : we deleted the initial notes .  made new db.json , ani append garyo . used useeffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import { AnecdoteForm } from "./components/AnecdoteForm";
 import { AnecdoteList } from "./components/AnecdoteList";
 import Notification from "./components/Notification";
 import Filter from "./components/Filter";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import anecdoteService from "./services/anecdoteService";
+import { appendNewAnecdote } from "./reducers/anecdoteReducer";
+
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((response) => dispatch(appendNewAnecdote(response)));
+  }, []);
   return (
     <div>
       <h2>Anecdotes</h2>
