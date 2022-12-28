dce9af42f771b38264d7123f29cf553945e95483
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-28 11:29:49 +0545

    feat : exercise 6.12 Modify the creation of new anecdotes, so that the anecdotes are stored in the backend.

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
+// const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
+// const asObject = (anecdote) => {
+//   return {
+//     content: anecdote,
+//     id: getId(),
+//     votes: 0,
+//   };
+// };
 
 // const initialState = anecdotesAtStart.map(asObject);
 
