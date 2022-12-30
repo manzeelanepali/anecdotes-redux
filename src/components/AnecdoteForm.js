d63394dc43bf4f8f87ad794178e621363a0d3690
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-30 14:46:09 +0545

    feat : making the code cleaner

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,28 +1,20 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
+
+import { connect } from "react-redux";
 
 import { addNewAnecdote } from "../reducers/anecdoteReducer";
 import anecdoteService from "../services/anecdoteService";
 import { updateNotification } from "../reducers/notificationReducer";
-export function AnecdoteForm() {
-  const dispatch = useDispatch();
 
-  //   const addAnecdotes = (event) => {
-  //     event.preventDefault();
-
-  //     const content = event.target.anecdote.value;
-
-  //     dispatch(addNewAnecdote(content));
-  //     event.target.anecdote.value = "";
-  //   };
-
+export function AnecdoteForm(props) {
   const addAnecdotes = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
     const newAnecdote = await anecdoteService.createNew(content);
-    dispatch(addNewAnecdote(newAnecdote));
-    dispatch(updateNotification(`added '${content}'`, 2));
+    props.addNewAnecdote(newAnecdote);
+    props.updateNotification(`added '${content}'`, 2);
     event.target.anecdote.value = "";
   };
 
@@ -38,3 +30,13 @@ export function AnecdoteForm() {
     </>
   );
 }
+const mapStateToProps = () => {
+  return {};
+};
+
+const mapDispatchToProps = {
+  addNewAnecdote,
+  updateNotification,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnecdoteForm);
